ce9959974e78ffcd5e2a335695bbdb8c7d3df27a
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-06-03 12:06:08 +0300

    And Custom Query example

--- src/main/java/com/marchenko/springboot/dmdev/repository/EmployeeRepository.java
+++ src/main/java/com/marchenko/springboot/dmdev/repository/EmployeeRepository.java
@@ -1,18 +1,30 @@
 package com.marchenko.springboot.dmdev.repository;
 
 import com.marchenko.springboot.dmdev.entity.EmployeeEntity;
+import com.marchenko.springboot.dmdev.projection.EmployeeNameView;
+import com.marchenko.springboot.dmdev.projection.EmployeeNativeView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer>, EmployeeCustomRepository {
 
     Optional<EmployeeEntity> findByFirstNameContaining(String firstName);
 
     @Query("select e from EmployeeEntity e where e.firstName = ?1 and e.salary = ?2")
     List<EmployeeEntity> findByFirstNameAndSalary(String firstName, Integer salary);
+
+    List<EmployeeNameView> findAllBySalaryGreaterThan(Integer salary);
+
+    @Query(value = "select " +
+            "e.id as id, " +
+            "e.first_name || e.last_name as fullName " +
+            "from employee e " +
+            "where  e.salary > :salary", nativeQuery = true)
+    List<EmployeeNativeView> findAllBySalaryGreaterNative(@Param("salary") Integer salary);
 }
--- src/test/java/com/marchenko/springboot/dmdev/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/marchenko/springboot/dmdev/repository/EmployeeRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.marchenko.springboot.dmdev.repository;
 
 import com.marchenko.springboot.dmdev.entity.EmployeeEntity;
+import com.marchenko.springboot.dmdev.projection.EmployeeNameView;
+import com.marchenko.springboot.dmdev.projection.EmployeeNativeView;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +36,23 @@ class EmployeeRepositoryTest extends IntegrationBaseTest {
     void testFindByFirstNameAndSalary() {
         List<EmployeeEntity> employees = employeeRepository.findByFirstNameAndSalary("Ivan", 1000);
         assertThat(employees, hasSize(1));
+    }
+
+    @Test
+    void testFindBySalary() {
+        List<EmployeeNameView> employees = employeeRepository.findAllBySalaryGreaterThan(500);
+        assertThat(employees, hasSize(2));
+    }
+
+    @Test
+    void testFindBySalaryNative() {
+        List<EmployeeNativeView> employees = employeeRepository.findAllBySalaryGreaterNative(500);
+        assertThat(employees, hasSize(2));
+    }
+
+    @Test
+    void testFindCustomQuery() {
+        List<EmployeeEntity> employees = employeeRepository.findCustomQuery();
+        assertThat(employees, hasSize(0));
     }
 }

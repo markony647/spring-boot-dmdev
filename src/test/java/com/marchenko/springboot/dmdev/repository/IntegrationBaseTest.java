d7ccb17cec1b49d28aaa21fa53dbc7bcabd2dc0c
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-06-03 13:21:12 +0300

    And TestContainers example

--- src/test/java/com/marchenko/springboot/dmdev/repository/IntegrationBaseTest.java
+++ src/test/java/com/marchenko/springboot/dmdev/repository/IntegrationBaseTest.java
@@ -1,13 +1,24 @@
 package com.marchenko.springboot.dmdev.repository;
 
 
+import com.marchenko.springboot.dmdev.initializer.Postgres;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import javax.transaction.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@ContextConfiguration(initializers = {
+        Postgres.Initializer.class
+})
 @Transactional
 public abstract class IntegrationBaseTest {
+
+    @BeforeAll
+    public static void init() {
+        Postgres.container.start();
+    }
 }
